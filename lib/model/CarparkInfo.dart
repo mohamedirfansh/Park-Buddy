efe41d01b94ddb19736edbf2f3fe27b09fc3e515
unknown <jireh_cwe@hotmail.com>
2021-02-26 03:31:26 +0800

    Add carpark availability data structure

--- lib/model/CarparkInfo.dart
+++ lib/model/CarparkInfo.dart
@@ -1,3 +1,5 @@
+import 'dart:core';
+import 'package:geodesy/geodesy.dart';
 import 'package:test_app/model/CarparkPaymentMethod.dart';
 import 'package:test_app/model/CarparkType.dart';
 import 'package:test_app/model/ShortTermParkingAvailability.dart';
@@ -6,18 +8,23 @@ class CarparkInfo {
   CarparkInfo(
       this.carparkCode,
       this.address,
-      this.lat,
-      this.lng,
+      this.latlng,
       this.carparkType,
       this.carparkPaymentMethod,
-      this.shortTermParking
+      this.shortTermParking,
       );
 
   final String carparkCode;
   final String address;
-  final double lat;
-  final double lng;
+  final LatLng latlng;
   final CarparkType carparkType;
   final CarparkPaymentMethod carparkPaymentMethod;
   final ShortTermParkingAvailability shortTermParking;
+}
+
+class CarparkAvailabilityInfo {
+  CarparkAvailabilityInfo(this.carparkCode, this.totalLots, this.availableLots);
+  final String carparkCode;
+  final num totalLots;
+  num availableLots;
 }
