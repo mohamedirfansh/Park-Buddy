04b3d4a4876766f178202005132b0d7a0a4ba5a2
unknown <jireh_cwe@hotmail.com>
2021-02-26 04:02:30 +0800

    Add search bar (no functionality and autocomplete yet).

---
+++ lib/view/carpark_info_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CarparkInfoPage extends StatefulWidget {
+  @override
+  _CarparkInfoPageState createState() => _CarparkInfoPageState();
+}
+
+class _CarparkInfoPageState extends State<CarparkInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//TODO: button to link to google maps app for directions (figure out function call from the default Marker onTap behaviour)
+//TODO: histogram (queried on demand, show current time data)
