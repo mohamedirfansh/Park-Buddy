cbf520e93e3a838a8c7ef9de73fb06a8c4559c0f
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2021-03-04 01:15:39 +0800

    json parsing working

--- lib/model/DatabaseManager.dart
+++ lib/model/DatabaseManager.dart
@@ -1,28 +1,32 @@
 import 'dart:convert';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
 class DatabaseManager {
   static Future<List<Carpark>> getAllEmployees() async {
     var url = "https://api.data.gov.sg/v1/transport/carpark-availability";
-    Response response = await Dio().get(url);
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final list = json.decode(response.body);
+      final items = list['items'];
 
-    return employeeFromJson((response.data as List)[0]);
+      return employeeFromJson(items[0]); //response.data as List
+    } else {
+      throw Exception("Failed to connect to server");
+    }
   }
 }
 
-List<Carpark> employeeFromJson(String str) {
-  List<dynamic> jsonList = json.decode(json.decode(str).sublist(1)); // carpark_data list
-  return List<Carpark>.from(jsonList.map((x) => Carpark.fromJson(x,jsonList[0])));
+List<Carpark> employeeFromJson(Map<String, dynamic> items) {
+  var timestamp = items["timestamp"];
+  var carparkData = items["carpark_data"];
+
+  return List<Carpark>.from(carparkData.map((x) => Carpark.fromJson(x, timestamp)));
 }
 
-//String employeeToJson(List<Carpark> data) =>
-  //  json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
-
 class Carpark {
-  String carparkID;
-  int totalLots;
+  var totalLots;
   String lotType;
-  int lotsAvailable;
+  var lotsAvailable;
   String carparkNumber;
   String updateDatetime;
   String timestamp;
@@ -36,12 +40,17 @@ class Carpark {
     this.timestamp,
 });
 
-  factory Carpark.fromJson(Map<String, dynamic> json, String timestamp) => Carpark(
-    timestamp: timestamp,
-    carparkNumber: json["carpark_number"],
-    totalLots: json["carpark_info"]["total_lots"],
-    lotType: json["carpark_info"]["lot_type"],
-    lotsAvailable: json["carpark_info"]["lots_available"],
-    updateDatetime: json["update_datetime"],
-  );
+  factory Carpark.fromJson(Map<String, dynamic> json, String timestamp) {
+    var carparkInfo = json["carpark_info"][0];
+    return Carpark(
+      timestamp: timestamp,
+      carparkNumber: json["carpark_number"],
+      totalLots: int.parse(carparkInfo["total_lots"]),
+      lotType: carparkInfo["lot_type"],
+      lotsAvailable: int.parse(carparkInfo["lots_available"]),
+      updateDatetime: json["update_datetime"],
+    );
+  }
 }
+
+// format: {"carpark_info":[{"total_lots":"91","lot_type":"C","lots_available":"33"}],"carpark_number":"HE12","update_datetime":"2021-03-03T22:49:23"}
