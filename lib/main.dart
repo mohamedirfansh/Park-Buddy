21bf6c842dd8573417a9a1dc65a4dca703eb63f4
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2021-03-04 01:18:17 +0800

    Added call to DatabaseManager; changed main to async

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,23 @@ import 'package:park_buddy/model/CarparkCSV.dart';
 import 'package:park_buddy/route_generator.dart';
 import 'package:park_buddy/view/SearchBar.dart';
 
-void main(){
+import 'model/DatabaseManager.dart';
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   //Preload csv data
   CarParkCSV.loadData();
   runApp(MyApp());
+  final list = await DatabaseManager.getAllEmployees();
+  /* test
+  print(list.length);
+  print(list[2073].carparkNumber);
+  print(list[2073].lotsAvailable);
+  print(list[2073].lotType);
+  print(list[2073].timestamp);
+  print(list[2073].totalLots);
+  print(list[2073].updateDatetime);
+   */
 }
 
 class MyApp extends StatelessWidget {
