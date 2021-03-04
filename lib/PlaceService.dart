4c7ecf3d17255486d772ef20712fbaf6a48ff3d5
unknown <jireh_cwe@hotmail.com>
2021-03-05 01:37:37 +0800

    Fix line endings for PlaceService.dart

--- lib/PlaceService.dart
+++ lib/PlaceService.dart
@@ -1,1 +1,66 @@
-import 'dart:convert';import 'package:geodesy/geodesy.dart';import 'package:http/http.dart';class Suggestion {  final String placeId;  final String description;  Suggestion(this.placeId, this.description);  @override  String toString() {    return 'Suggestion($placeId, $description)';  }}class PlaceApiProvider {  final client = Client();  PlaceApiProvider(this.sessionToken);  final sessionToken;  final apiKey = 'AIzaSyAZgPwhId1JCrLpMzgCfPABeGYV8Fkso-U';  Future<List<Suggestion>> fetchSuggestions(String input, String lang) async {    final request =        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$input&language=$lang&components=country:sg&key=$apiKey&sessiontoken=$sessionToken';    final response = await client.get(request);    if (response.statusCode == 200) {      final result = json.decode(response.body);      if (result['status'] == 'OK') {        // compose suggestions in a list        return result['predictions']            .map<Suggestion>((p) => Suggestion(p['place_id'], p['description']))            .toList();      }      if (result['status'] == 'ZERO_RESULTS') {        print("no results");        return [];      }      throw Exception(result['error_message']);    } else {      throw Exception('Failed to fetch suggestion');    }  }  Future<LatLng> getPlaceLatLngFromId(String placeId) async {    final request =        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&fields=geometry/location&key=$apiKey&sessiontoken=$sessionToken';    final response = await client.get(request);    if (response.statusCode == 200) {      final result = json.decode(response.body);      if (result['status'] == 'OK') {        return LatLng(result['result']['geometry']['location']['lat'], result['result']['geometry']['location']['lng']);      }      throw Exception(result['error_message']);    } else {      throw Exception('Failed to fetch suggestion');    }  }}
+import 'dart:convert';
+
+import 'package:geodesy/geodesy.dart';
+import 'package:http/http.dart';
+
+class Suggestion {
+  final String placeId;
+  final String description;
+
+  Suggestion(this.placeId, this.description);
+
+  @override
+  String toString() {
+    return 'Suggestion($placeId, $description)';
+  }
+
+}
+
+class PlaceApiProvider {
+  final client = Client();
+
+  PlaceApiProvider(this.sessionToken);
+
+  final sessionToken;
+  final apiKey = 'AIzaSyAZgPwhId1JCrLpMzgCfPABeGYV8Fkso-U';
+
+  Future<List<Suggestion>> fetchSuggestions(String input, String lang) async {
+    final request =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$input&language=$lang&components=country:sg&key=$apiKey&sessiontoken=$sessionToken';
+    final response = await client.get(request);
+
+    if (response.statusCode == 200) {
+
+      final result = json.decode(response.body);
+      if (result['status'] == 'OK') {
+        // compose suggestions in a list
+        return result['predictions']
+            .map<Suggestion>((p) => Suggestion(p['place_id'], p['description']))
+            .toList();
+      }
+      if (result['status'] == 'ZERO_RESULTS') {
+        print("no results");
+        return [];
+      }
+      throw Exception(result['error_message']);
+    } else {
+      throw Exception('Failed to fetch suggestion');
+    }
+  }
+
+  Future<LatLng> getPlaceLatLngFromId(String placeId) async{
+    final request =
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&fields=geometry/location&key=$apiKey&sessiontoken=$sessionToken';
+    final response = await client.get(request);
+
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);
+      if (result['status'] == 'OK') {
+        return LatLng(result['result']['geometry']['location']['lat'], result['result']['geometry']['location']['lng']);
+      }
+      throw Exception(result['error_message']);
+    } else {
+      throw Exception('Failed to fetch suggestion');
+    }
+  }
+}
