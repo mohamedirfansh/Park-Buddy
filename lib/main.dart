2dc46fa3797f6e7d278d669d2a5e3c11b0cd3203
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2021-03-06 18:36:49 +0800

    Include database test cases inside main method

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:park_buddy/view/TabsPage.dart';
 import 'package:park_buddy/model/CarparkCSV.dart';
 import 'package:park_buddy/route_generator.dart';
 import 'package:park_buddy/view/SearchBar.dart';
+import 'package:sqflite/sqflite.dart';
 
+import 'model/AvailabilityDatabase.dart';
 import 'model/DatabaseManager.dart';
 
 void main() async{
@@ -11,8 +13,8 @@ void main() async{
   //Preload csv data
   CarParkCSV.loadData();
   runApp(MyApp());
-  final list = await DatabaseManager.getAllEmployees();
-  /* test
+  final list = await DatabaseManager.pullCarparkAvailability(DateTime.now());
+  /*
   print(list.length);
   print(list[2073].carparkNumber);
   print(list[2073].lotsAvailable);
@@ -20,7 +22,11 @@ void main() async{
   print(list[2073].timestamp);
   print(list[2073].totalLots);
   print(list[2073].updateDatetime);
-   */
+  */
+  await AvailabilityDatabase.instance.getAllCarparks();
+  await AvailabilityDatabase.instance.deleteAllCarparkBefore(DateTime.now());
+  print("new query");
+  await AvailabilityDatabase.instance.getAllCarparks();
 }
 
 class MyApp extends StatelessWidget {
