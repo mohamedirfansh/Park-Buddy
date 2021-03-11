0fadc9143daca51a93d64c8aa910946641637214
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2021-03-11 22:54:22 +0800

    Add files via upload

--- lib/model/DatabaseManager.dart
+++ lib/model/DatabaseManager.dart
@@ -7,7 +7,7 @@ import 'CarparkAvailability.dart';
 import 'CarparkAPIInterface.dart';
 
 class DatabaseManager {
-
+  /*
   static HashSet<String> _duplicateSet = HashSet.from([
     "SK24", "STM2", "U11", "SK34", "SK35", "GBM", "KB1", "SB26", "SB27", "SB28", "S28L", "PM13", "Y48", "Y48M", "SB18", "SB21", "Y12", "Y6", "Y34", "Y34A", "Y36",
     "Y40", "Y43", "MPS", "Y10", "Y11", "Y16", "Y18", "Y19", "Y20", "Y21", "Y21M", "Y26", "Y27", "Y28", "Y28M", "Y29", "Y3", "Y33", "Y39", "Y3M", "Y4", "Y41", "Y41M",
@@ -17,7 +17,7 @@ class DatabaseManager {
     "SB1", "SB13", "SB16", "SB19", "SB22", "SB23", "SB24", "SB29", "SB3", "SB34", "SB5", "SB6", "SB8", "SB9", "SB30", "SB31", "SB33", "SB11", "S15L",
     "SB12", "SB15", "SB4", "SB7", "SB2", "Y13", "SB10", "Y14", "Y15", "BL8", "BL8L", "MNM", "SK82", "SK84", "TBM2", "H8", "C3M", "ACM", "C24", "C25", "C22M", "GE2"
   ]);
-
+*/
 
   /**
       static Future<List<CarparkAvailability>> pullCarparkAvailability() async {
@@ -40,24 +40,25 @@ class DatabaseManager {
 
   /// private method to convert retrieved carpark availability json into CarparkAvailability object
   static Future<List<CarparkAvailability>> _availabilityFromJson(Map<String, dynamic> items) async {
-    //HashSet<String> duplicateSet = new HashSet<String>();
+    HashSet<String> duplicateSet = new HashSet<String>();
     var timestamp = items["timestamp"];
     var carparkData = items["carpark_data"];
 
     var carparkList = List<CarparkAvailability>.from(carparkData.map((x) {
       CarparkAvailability item = CarparkAvailability.fromJson(x, timestamp);
-      if (!_duplicateSet.contains(item.carparkNumber)) {
-        //duplicateSet.add(item.carparkNumber);
+      if (!duplicateSet.contains(item.carparkNumber)) {
+        duplicateSet.add(item.carparkNumber);
         return item;
       }
     }
     )
     );
-    //var count = carparkList.where((c) => c!= null && c.carparkNumber == "STM1").toList().length;
-    //print(count);
     await AvailabilityDatabase.instance.batchInsertCarparks(carparkList);
   }
 }
 // query: https://api.data.gov.sg/v1/transport/carpark-availability?date_time=2020-03-05T01%3A40%3A27
 
 // format: {"carpark_info":[{"total_lots":"91","lot_type":"C","lots_available":"33"}],"carpark_number":"HE12","update_datetime":"2021-03-03T22:49:23"}
+// other format: {"carpark_info":[{"total_lots":"73","lot_type":"C","lots_available":"0"},{"total_lots":"12","lot_type":"Y","lots_available":"2"},
+//                {"total_lots":"50","lot_type":"H","lots_available":"50"}],"carpark_number":"K2T","update_datetime":"2021-03-11T20:19:17"}
+// "carpark_number":"SK24","update_datetime":"2021-03-11T20:30:08"},{"carpark_info":[{"total_lots":"181","lot_type":"Y","lots_available":"200"}],"carpark_number":"SK24","update_datetime":"2016-02-19T11:19:28"},{"carpark_info":[{"total_lots":"181","lot_type":"H","lots_available":"200"}],"carpark_number":"SK24","update_datetime":"2016-02-19T11:19:29"},{"carpark_info":[{"total_lots":"225","lot_type":"C","lots_available":"61"}]
