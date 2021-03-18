84b670d13b86374ffde2ae19d55855e5c20079aa
zvarellalee <62020585+zvarellalee@users.noreply.github.com>
2021-03-19 01:19:48 +0800

    Remove DatabaseManager calls

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:park_buddy/view/TabsPage.dart';
 import 'package:park_buddy/model/CarparkCSV.dart';
 import 'package:park_buddy/route_generator.dart';
-import 'package:park_buddy/view/MapViewWithSearch.dart';
-
-import 'model/DatabaseManager.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   //Preload csv data
   CarParkCSV.loadData();
-  await DatabaseManager.pullCarparkAvailability(DateTime.now());
-  await DatabaseManager.printAllCarparks();
-  await DatabaseManager.deleteAllCarparkBefore(DateTime.now());
-  await DatabaseManager.printAllCarparks();
-
   runApp(MyApp());
+  /*
+  await DatabaseManager.deleteAllCarparkBefore(DateTime.now().toUtc().add(Duration(hours:8)));
+  print(DateTime.now().millisecondsSinceEpoch);
 
+  await DatabaseManager.printAllCarparks();
+  final stopwatch = Stopwatch()..start();
+  await PullDateManager.pullMissingDates();
+  stopwatch.stop();
+  print('pullMissingDates() executed in ${stopwatch.elapsed}');
+  */
 }
 
 class MyApp extends StatelessWidget {
